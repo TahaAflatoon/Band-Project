e0b6e463fb65f9d04ffaec7bbf2a0d2cc16fdbc1
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-10-22 20:35:22 +0330

    improved syntax & readability

--- band.js
+++ band.js
@@ -1,18 +1,18 @@
-/*JQuery*/
+// JQuery
 $.noConflict()
 jQuery(document).ready(function($) {
-    //search input
+    // Search input
     $('.fa-search').click(function() {
         $('.search-input').toggle(500);
     }),
-    //dropdown button
+    // Dropdown button
     $('.mobile-dropbtn').click(function() {
         $('.mobile-content').slideToggle(500)
     })
 })
 
-/*JavaScript*/
-//Sticky menu background
+// JavaScript
+// Sticky menu background
 window.addEventListener('scroll', function() {
     if (window.scrollY > 51) {
         document.querySelector('.header').style.opacity = 0.85;
@@ -21,19 +21,20 @@ window.addEventListener('scroll', function() {
     }
 });
 
-//slideshow
+// Slideshow
 function slideshow() {
     let slideIndex = 0;
     showSlides();
     function showSlides() {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
+    const slides = document.querySelectorAll('.mySlides');
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = 'none';
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = 'block';
     setTimeout(showSlides, 1500); // Change image every 1.5 seconds
     }
-}
+};
+slideshow();
